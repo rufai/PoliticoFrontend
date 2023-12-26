159188386616a33fd4d91fffa80c5973b99dc286
Rufai Mustapha <etop94@gmail.com>
2023-12-26 10:34:21 +0100

    created a fetch from supabase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.supabase_url,
+  process.env.supabase_anon_key
+);
 
 function App() {
+  const [party, setPaties] = useState([]);
+
+  useEffect(() => {
+    getParties();
+  }, []);
+
+  async function getParties() {
+    console.log(supabase);
+    const { data } = await supabase.from("Party").select().limit(10);
+    console.log(data);
+    setPaties(data);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Politico Frontend</p>
+
+        <p> checking </p>
+        <p> {party} </p>
       </header>
+
+      <section>
+        <p>
+          <a href="">sign-in</a>
+        </p>
+        <p>
+          <a href="">sign-out</a>
+        </p>
+      </section>
     </div>
   );
 }
